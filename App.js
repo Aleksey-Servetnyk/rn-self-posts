d1e9fcbfdc95e401c0adaa5d7c6e6d9d18b43c37
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2020-11-03 10:20:27 +0200

    creating of the application ended

--- App.js
+++ App.js
@@ -2,12 +2,19 @@ import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { AppLoading } from "expo";
+import { bootstrap } from "./src/bootstrap";
 
 export default function App() {
   const [isRedy, setIsRedy] = useState(false);
 
   if (!isRedy) {
-    return <AppLoading />;
+    return (
+      <AppLoading
+        startAsync={bootstrap}
+        onFinish={() => setIsRedy(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
   }
 
   return (
