d5d891dce1fbc362a682402400818fac0183c5d6
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2020-11-05 05:54:20 +0200

    styling PostScreen ended

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,20 +1,60 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Image,
+  Text,
+  Button,
+  ScrollView,
+  Alert
+} from "react-native";
+import { DATA } from "../data";
+import { THEME } from "../theme";
 
 export const PostScreen = ({ route, navigation }) => {
-  const postId  = route.params.postId;
+  const postId = route.params.postId;
+
+  const post = DATA.find((p) => p.id === postId);
+
+  const removeHandler = () => {
+    Alert.alert(
+      "Deleting the post",
+      "Are You sure?",
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        { text: "Delete", style: "positive", onPress: () => {} },
+      ],
+      { cancelable: false }
+    );
+  };
   //console.log({ navigation }, { postId }, {route});
   return (
-    <View style={styles.center}>
-      <Text>{postId}</Text>
-    </View>
+    <ScrollView>
+      <Image source={{ uri: post.img }} style={styles.image} />
+      <View style={styles.textWrap}>
+        <Text style={styles.title}>{post.text}</Text>
+      </View>
+      <Button
+        title="Delete"
+        color={THEME.DANGER_COLOR}
+        onPress={removeHandler}
+      />
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  textWrap: {
+    padding: 10,
+  },
+  title: {
+    fontFamily: "open-regular",
   },
 });
