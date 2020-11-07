baee13b153a1e3d498d4e1f459b27d8fb2206162
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2020-11-07 05:36:07 +0200

    setting the font to menu items & menu styling ended

--- src/navigation/navigators/MainNavigator.js
+++ src/navigation/navigators/MainNavigator.js
@@ -1,14 +1,22 @@
-import { createDrawerNavigator } from '@react-navigation/drawer'
+import {createDrawerNavigator} from '@react-navigation/drawer'
 import React from "react";
 import {BottomNavigator} from "./BottomNavigator";
 import {AboutStackScreen} from "../stacks/AboutStackScreen";
 import {CreateStackScreen} from "../stacks/CreateStackScreen";
+import {THEME} from "../../theme";
 
 const Drawer = createDrawerNavigator();
 
 export function MainNavigator() {
     return (
-        <Drawer.Navigator>
+        <Drawer.Navigator
+            drawerContentOptions={{
+                activeTintColor: THEME.MAIN_COLOR,
+                labelStyle: {
+                    fontFamily: 'open-regular'
+                }
+            }}
+        >
             <Drawer.Screen
                 name="PostTabs"
                 component={BottomNavigator}
