35752a72e60b9a529d05be463015af52ba0406d1
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2020-11-07 05:48:41 +0200

    the navigation ended

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,18 +1,22 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {View, Text, StyleSheet} from "react-native";
 
 export const AboutScreen = ({}) => {
-  return (
-    <View style={styles.center}>
-      <Text>About Screen</Text>
-    </View>
-  );
+    return (
+        <View style={styles.center}>
+            <Text>It`s best application for You own posts.</Text>
+            <Text>Application Version<Text style={styles.version}> 1.0.0.</Text></Text>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
+    center: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    version: {
+        fontFamily: 'open-bold'
+    }
 });
