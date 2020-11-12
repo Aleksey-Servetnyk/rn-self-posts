5ad8d774e86f03781c71a1108b355afdd0484ebf
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2020-11-12 18:33:29 +0200

    preparing the app to use with a db ended

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,21 +1,34 @@
 import React from "react";
-import { View, StyleSheet, FlatList } from "react-native";
+import {View, StyleSheet, FlatList, Text} from "react-native";
 import {Post} from "./Post";
 
-export const PostList = ({ data, onOpen }) => {
-  return (
-    <View styles={styles.wrapper}>
-      <FlatList
-        data={data}
-        keyExtractor={(post) => post.id.toString()}
-        renderItem={({ item }) => <Post post={item} onOpen={onOpen} />}
-      />
+export const PostList = ({data, onOpen}) => {
+
+if (!data.length) {
+    return <View style={styles.wrapper}>
+        <Text style={styles.noItems}>No posts yet</Text>
     </View>
-  );
+}
+
+    return (
+        <View styles={styles.wrapper}>
+            <FlatList
+                data={data}
+                keyExtractor={(post) => post.id.toString()}
+                renderItem={({item}) => <Post post={item} onOpen={onOpen}/>}
+            />
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  wrapper: {
-    padding: 10,
-  },
+    wrapper: {
+        padding: 10,
+    },
+    noItems: {
+        fontFamily: 'open-regular',
+        textAlign: 'center',
+        marginVertical: 10,
+        fontSize: 18
+    }
 });
