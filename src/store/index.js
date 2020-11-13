3c81b2f1d3d81c76653cf871e7a7137645ab7a7a
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2020-11-13 07:03:37 +0200

    getting all posts from the db

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
-import {combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore} from "redux";
 import {postReducer} from "./reducers/post";
+import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
     post: postReducer
 })
 
-export default createStore(rootReducer)
+export default createStore(rootReducer, applyMiddleware(thunk))
