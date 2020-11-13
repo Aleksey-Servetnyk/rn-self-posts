388352623d3b90c40158deac633be9f57d244f63
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2020-11-13 07:43:06 +0200

    creating a new post to db and saving an image file to document directory

--- src/store/actions/post.js
+++ src/store/actions/post.js
@@ -1,5 +1,6 @@
 import {ADD_POST, LOAD_POSTS, REMOVE_POST, TOGGLE_BOOKED} from "../types";
 import {DB} from "../../db";
+import * as FileSystem from 'expo-file-system';
 
 export const loadPosts = () => {
     return async (dispatch) => {
@@ -24,11 +25,26 @@ export const removePost = (id) => {
     }
 }
 
-export const addPost = (post) => {
-    post.id = Date.now().toString()
-    return {
-        type: ADD_POST,
-        payload: post
+export const addPost = (post) => async (dispatch) => {
+    const fileName = post.img.split('/').pop()
+    const newPath = FileSystem.documentDirectory + fileName
+
+    try {
+        await FileSystem.moveAsync({
+            to: newPath,
+            from: post.img
+        })
+    } catch (e) {
+        console.log('Error: ', e)
     }
+
+    const payload = {...post, img: newPath}
+    const id = await DB.createPost(payload)
+    payload.id = id
+
+    dispatch({
+        type: ADD_POST,
+        payload: payload
+    })
 }
 
